c9fabc36e10c2e8f97f8c144ea26658fd369126f
AniketWhoCodes <aniketkolte18@gmail.com>
2024-07-08 20:59:59 +0530

    feature: Added skills section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import "./App.css";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contanct ";
+import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
+import Projects from "./components/Projects/Project";
+import Skills from "./components/Skills/Skills";
+import FixSocialIcon from "./components/SocialIcons/FixSocialIcon";
+import ScrollToTop from "./components/SocialIcons/ScrollToTop";
 
 export default function App() {
   return (
     <>
       <Hero />
+      <Projects />
+      <About />
+      <Skills />
+      <Contact />
+      <FixSocialIcon />
+      <Footer />
+      <ScrollToTop />
     </>
   );
 }
--- src/data/Data.ts
+++ src/data/Data.ts
@@ -17,3 +17,92 @@ export const ProjectList = [
     },
 
 ];
+
+export const stackList = [
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+        name: "HTML",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+        name: "CSS",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg",
+        name: "Sass",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+        name: "JavaScript",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+        name: "Typescript",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/angular/angular-original.svg",
+        name: "Angular",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+        name: "ReactJS",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/angularmaterial/angularmaterial-original.svg",
+        name: "Angular Mat",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
+        name: "Material UI",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-original.svg",
+        name: "TailwindCSS",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+        name: "Bootstrap",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/antdesign/antdesign-original.svg",
+        name: "Ant Design",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        name: "Java",
+    },
+
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/spring/spring-original.svg",
+        name: "SpringBoot",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original.svg",
+        name: "Docker",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original.svg",
+        name: "PostgreSQL",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-plain.svg",
+        name: "Firebase",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+        name: "MySQL",
+    },
+
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/postman/postman-original.svg",
+        name: "Postman",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/intellij/intellij-original.svg",
+        name: "Intellij Idea",
+    },
+    {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+        name: "VS Code",
+    },
+];
